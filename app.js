b6aaed93441f038c2b258043e888cfcf898dabe3
Anderson Siqueira <andersonboyjua@gmail.com>
2022-03-09 13:25:56 -0300

    Menu mini refectore

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
 // menu responsivo 
 
-const showMenuToggle = document.querySelectorAll('.menu_toggle')
+const showMenuToggle = document.querySelector('.menu_toggle')
 const onMenu = document.querySelector('#on_full_Menu')
 const one = document.querySelector('.one')
 const two = document.querySelector('.two')
 const three = document.querySelector('.three')
 
-Array.from(showMenuToggle).forEach(element => {
-    element.addEventListener('click', event => {
+
+    showMenuToggle.addEventListener('click', event => {
         onMenu.classList.toggle('showOff')
         one.classList.toggle('onOne')
         two.classList.toggle('onTwo')
         three.classList.toggle('onThree')
     })
-})
+
 
 
 console.log(two)
